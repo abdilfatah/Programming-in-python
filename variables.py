c20aab7c25eaab7937ae3dfb45b45c0569a55c13
Abdifatah Ibrahim <guhanahmed@gmail.com>
2025-01-14 17:48:13 +0300

    Python variables is added

---
+++ variables.py
@@ -0,0 +1,33 @@
+x = 10 # variable x is assigned the value 10
+
+# Naming Convension
+myName = "John" # camel case
+my_name = "John" # snake case
+MyName = "John" # pascal case
+
+# Declaring Variable
+
+x = "Hello" # automatically assigns the data type as string
+
+print(x)
+
+a = b = c = 10 # assigns the value 10 to all the variables
+print(a)
+print(b)
+print(c)
+
+a, b, c = 10, 20, 30 # assigns the value 10 to a, 20 to b and 30 to c
+print(a)
+print(b)
+print(c)
+
+a = 10
+print(a)
+
+a = 5
+print(a)
+
+
+del a # deletes the variable a
+
+# print(a) # NameError: name 'a' is not defined
