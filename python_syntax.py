e530a5388b146108b6545109e8c75615d5bf7261
Abdifatah Ibrahim <guhanahmed@gmail.com>
2025-01-14 17:32:38 +0300

    Python Syntax is added

---
+++ python_syntax.py
@@ -0,0 +1,8 @@
+x = 1 +        2 \
++ 3   # the backward slash is used to continue the statement in the next line 
+print(x)  # 6
+
+name = "John"
+
+if name == "John":
+    print(name)  # indentation is important in Python
