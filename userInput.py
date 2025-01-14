4051a9f9ed67bac03b7066b77df2260d003a1c37
Abdifatah Ibrahim <guhanahmed@gmail.com>
2025-01-14 18:49:26 +0300

    Python userinput is added

---
+++ userInput.py
@@ -0,0 +1,11 @@
+# Helper functions for user input
+
+email = input("Enter your email: ")
+print("Your email is: " + email)
+
+num1 = input("Enter a number: ")	
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+print(result)
+
+# The input() function allows user input. The input is always a string. To convert the input to integer or float, use int() or float() functions respectively.
